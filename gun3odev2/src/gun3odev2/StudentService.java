200cb88524b7fc289bde097b5e99bcdaeb9fd221
Doğancan Nurlu <nurludogancan@gmail.com>
2021-09-08 19:32:57 +0300

    ty

--- gun3odev2/src/gun3odev2/StudentService.java
+++
@@ -1,9 +0,0 @@
-package gun3odev2;
-
-public class StudentService extends UserService {
-	
-	public void gradeAdd(Student student) {
-		System.out.println("Öðrencinin notu sisteme eklendi: " + student.getGrade());
-	}
-
-}
